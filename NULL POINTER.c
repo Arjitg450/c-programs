0c394a6aadda740016250f5be34eb2b8067c495d
Arjit Gupta <37619926+Arjitg450@users.noreply.github.com>
2019-11-04 09:33:44 +0530

    Add files via upload

---
+++ NULL POINTER.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main(){
+int *a=NULL;
+printf("%u",a);
+struct date{
+int d,m,y;
+};
+int *addd;
+struct date arjitbd;
+addd=&arjitbd;
+printf("%u",addd);
+
+
+
+}
---
+++ malloc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    int *p;
+    p=malloc(4);
+    scanf("%d",p);
+    printf("%d",*p);
+
+
+
+}
